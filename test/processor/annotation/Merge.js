a42421100833eaaf2c3478eefe192f7b3c00063b
Maximiliano Fierro <elmasse@gmail.com>
2013-04-18 20:06:05 -0700

    Adding new merge strategies: deep-mine and deep-their

--- test/processor/annotation/Merge.js
+++ test/processor/annotation/Merge.js
@@ -136,7 +136,7 @@ describe('Annotation Processor @merge', function(){
                 expect(Subject).to.respondTo('method');
             });
 
-            it('do NOT override properties and/or methods defined in subject object if they have the same name', function(){
+            it('does NOT override properties and/or methods defined in subject object if they have the same name', function(){
                 var aMethod = function(){},
                     subject = {
                         property: 0,
@@ -157,7 +157,7 @@ describe('Annotation Processor @merge', function(){
                 expect(subject.override).to.be.equal(aMethod);
             });
 
-            it('do NOT override properties and/or methods defined in Subject class if they have the same name', function(){
+            it('does NOT override properties and/or methods defined in Subject class if they have the same name', function(){
                 var aMethod = function(){},
                     Subject = function(){},
                     proto = {
@@ -178,7 +178,138 @@ describe('Annotation Processor @merge', function(){
                 expect(Subject.prototype.override).to.be.equal(aMethod);
             });
 
+        });
 
+
+        describe('`deep-mine` merge strategy', function(){
+            var strategy = "deep-mine";
+
+            it('adds properties and/or methods defined in proto into subject object', function(){
+                var subject = {},
+                    proto = {
+                        property: 1,
+                        method: function(){}
+                    };
+
+                sut.setParameter(strategy);
+                sut.process(subject, proto);
+
+                expect(subject).to.have.property('property').that.equal(1);
+                expect(subject).to.respondTo('method');
+            });
+
+            it('adds properties and/or methods defined in proto into Subject class', function(){
+                var Subject = function(){},
+                    proto = {
+                        property: 1,
+                        method: function(){}
+                    };
+
+                sut.setParameter(strategy);
+                sut.process(Subject, proto);
+
+                expect(Subject.prototype).to.have.property('property').that.equal(1);
+                expect(Subject).to.respondTo('method');
+            });
+
+            it('overrides properties defined in subject object if they have the same name, if the target property is an object it merges the content using strategy mine', function(){
+                var subject = {
+                        property: {
+                            sub1 : 0,
+                            sub2: 1
+                        }
+                    },
+                    proto = {
+                        property: {
+                            sub1: 1
+                        }
+                    };
+
+                sut.setParameter(strategy);
+                sut.process(subject, proto);
+
+                expect(subject.property).to.have.property('sub1').that.equal(1);
+                expect(subject.property).to.have.property('sub2').that.equal(1);
+            });
+
+            it('overrides properties defined in subject object if they have the same name, if the target property is an array it concatenates the content', function(){
+                var subject = {
+                        arr: [1,2,3]
+                    },
+                    proto = {
+                        arr: [4,5,6]
+                    };
+
+                sut.setParameter(strategy);
+                sut.process(subject, proto);
+                expect(subject.arr).to.eql([1,2,3,4,5,6]);
+            });
+        });
+
+        describe('`deep-their` merge strategy', function(){
+            var strategy = "deep-their";
+
+            it('adds properties and/or methods defined in proto into subject object', function(){
+                var subject = {},
+                    proto = {
+                        property: 1,
+                        method: function(){}
+                    };
+
+                sut.setParameter(strategy);
+                sut.process(subject, proto);
+
+                expect(subject).to.have.property('property').that.equal(1);
+                expect(subject).to.respondTo('method');
+            });
+
+            it('adds properties and/or methods defined in proto into Subject class', function(){
+                var Subject = function(){},
+                    proto = {
+                        property: 1,
+                        method: function(){}
+                    };
+
+                sut.setParameter(strategy);
+                sut.process(Subject, proto);
+
+                expect(Subject.prototype).to.have.property('property').that.equal(1);
+                expect(Subject).to.respondTo('method');
+            });
+
+            it('overrides properties defined in subject object if they have the same name, if the target property is an object it merges the content using strategy their', function(){
+                var subject = {
+                        property: {
+                            sub1 : 0
+                            
+                        }
+                    },
+                    proto = {
+                        property: {
+                            sub1: 1,
+                            sub2: 1
+                        }
+                    };
+
+                sut.setParameter(strategy);
+                sut.process(subject, proto);
+
+                expect(subject.property).to.have.property('sub1').that.equal(0);
+                expect(subject.property).to.have.property('sub2').that.equal(1);
+            });
+
+            it('overrides properties defined in subject object if they have the same name, if the target property is an array it concatenates the content', function(){
+                var subject = {
+                        arr: [1,2,3]
+                    },
+                    proto = {
+                        arr: [4,5,6]
+                    };
+
+                sut.setParameter(strategy);
+                sut.process(subject, proto);
+                expect(subject.arr).to.eql([1,2,3,4,5,6]);
+            });
         });
 
     });
